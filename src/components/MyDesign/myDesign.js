f353f0e639bc1d574343f2387b245ce6d3abf838
raisedproper <38512946+raisedproper@users.noreply.github.com>
2019-01-10 21:20:58 -0800

    Update myDesign.js

--- src/components/MyDesign/myDesign.js
+++ src/components/MyDesign/myDesign.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
+
 
 
 class MyDesign extends Component {
