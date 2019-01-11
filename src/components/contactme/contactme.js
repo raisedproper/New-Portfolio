c86b117ede62411edc2560bbcde24ae1368899d2
raisedproper <38512946+raisedproper@users.noreply.github.com>
2019-01-11 00:43:37 -0800

    Update contactme.js

--- src/components/contactme/contactme.js
+++ src/components/contactme/contactme.js
@@ -10,6 +10,9 @@ class ContactMe extends Component {
                         <p className="mainFont2">Contact Me</p>
                             <div>
                                 <a className="subFont" href={"mailto:" + this.props.email}>DannyMorris188@gmail.com</a>
+                                  <br><br/>
+                                <a className="subFont" href="https://www.linkedin.com/in/danny-morris-29a936167/">Linkedin</a>
+
                             </div>      
                     </div>
                 </div> 
